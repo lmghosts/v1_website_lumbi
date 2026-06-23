d4addc93f65f1015158a64b99ac3d7c773ff7f86
lmghosts <muchuilumbi@gmail.com>
2026-06-23 17:24:51 +0300

    fix: render viewport metadata during SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require('react');
 
- // You can delete this file if you're not using it
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    React.createElement('meta', {
+      key: 'viewport',
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1',
+    }),
+  ]);
+};
